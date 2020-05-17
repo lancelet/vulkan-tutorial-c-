5e15cad08101f25400f93a62f66cd3f34197ed70
Jonathan Merritt <j.s.merritt@gmail.com>
2020-05-17 12:50:23 +1000

    Queue families

--- hello-triangle/hello-triangle.cpp
+++ hello-triangle/hello-triangle.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -49,6 +50,14 @@ void DestroyDebugUtilsMessengerEXT
     func(instance, debugMessenger, pAllocator);
   }
 }
+
+struct QueueFamilyIndices {
+  std::optional<uint32_t> graphicsFamily;
+
+  bool isComplete() {
+    return graphicsFamily.has_value();
+  }
+};
 
 class HelloTriangleApplication {
 public:
@@ -233,7 +242,32 @@ private:
   }
 
   bool isDeviceSuitable(VkPhysicalDevice device) {
-    return true;
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
+  }
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i=0;
+    for (const auto& queueFamily : queueFamilies) {
+      if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        indices.graphicsFamily = i;
+      }
+      if (indices.isComplete()) {
+        break;
+      }
+      i++;
+    }
+
+    return indices;
   }
 
   void mainLoop() {
